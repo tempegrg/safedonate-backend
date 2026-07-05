b0f8d6e8a9a0d6079a24c3fe0c9e92d86595e0a1
tempegrg <aunizzaty75@gmail.com>
2026-07-05 21:00:45 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -80,3 +80,13 @@ Route::put(
     '/organisation-applications/{id}/reject',
     [OrganisationApplicationController::class, 'reject']
 );
+
+Route::get('/debug-db', function () {
+    return response()->json([
+        'DB_CONNECTION' => env('DB_CONNECTION'),
+        'DB_HOST' => env('DB_HOST'),
+        'DB_PORT' => env('DB_PORT'),
+        'DB_DATABASE' => env('DB_DATABASE'),
+        'DB_USERNAME' => env('DB_USERNAME'),
+    ]);
+});
